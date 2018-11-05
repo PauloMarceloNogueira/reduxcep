fd547985ccd5d95452e36549b69a8ca7e283f719
Paulo Marcelo <paulomarceloa.nogueira@gmail.com>
2018-11-05 15:29:17 -0200

    Fix CEP

--- src/Components/Map/FrameMap.jsx
+++ src/Components/Map/FrameMap.jsx
@@ -4,16 +4,23 @@ import { connect } from 'react-redux';
 import { cep_changed } from '../Input/InputActions.js'
 import './Map.sass'
 const FrameMap = (props) => {
+  console.log(props)
+  let cepToSearch
+  console.log(cepToSearch)
+  if (props.cep) {
+    cepToSearch = props.cep.replace('-','');
+  }
   return (
     <div className="map-frame">
       <iframe width='100%' height='350' frameBorder='0'  
         scrolling='no' marginHeight='0' marginWidth='0'    
-        src={`https://maps.google.com/maps?q=${props.logradouro}&output=embed`}></iframe>
+        src={`https://maps.google.com/maps?q=${cepToSearch}&output=embed`}></iframe>
     </div>
   )
 }
 const mapStateToProps = (state) => ({
   logradouro: state.input.logradouro,
+  cep: state.input.cep
 });
 const mapDispatchToProps = (dispatch) => bindActionCreators({ cep_changed }, dispatch);
 
